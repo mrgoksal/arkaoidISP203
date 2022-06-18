5d54b598975f8456574669e1c7d6d6aae1ba4ada
mrgoksal <59969899+mrgoksal@users.noreply.github.com>
2022-06-18 07:59:58 +0300

    Add files via upload

--- configs.py
+++ configs.py
@@ -1,28 +1,28 @@
 import pygame
 from random import randrange as rnd
 
-# game settings
-WIDTH = 1600
-HEIGHT = 900
-SCALE = WIDTH / 1920
-FPS = 60
+# Настройки окна
+WIDTH = 1920  # Ширина
+HEIGHT = 1080  # Высота
+SCALE = WIDTH / 1920  # Коэффициент скейла
+FPS = 60  # Тикрейт (частота спроса)
 
-# paddle settings
-PADDLE_W = 330
-PADDLE_H = 15
-PADDLE_SPEED = 20
+# Настройки "отбивной" доски
+PADDLE_W = 330  # Ширина
+PADDLE_H = 35  # Высота
+PADDLE_SPEED = 15  # Скорость перемещения
 
-# ball settings
-BALL_RADIUS = 10
-BALL_SPEED = 6
+# Настройки мяча
+BALL_RADIUS = 20  # Радиус
+BALL_SPEED = 6  # Скорость полета
 
-# blocks settings
-BLOCKS_WIDTH = 100
-BLOCKS_HEIGHT = 25
-BLOCKS_DIST = 20
-BLOCKS_LENGTH = BLOCKS_WIDTH + BLOCKS_DIST
-BLOCKS_COUNT = WIDTH // BLOCKS_LENGTH
+# Настройки  блоков
+BLOCKS_WIDTH = 192  # Ширина
+BLOCKS_HEIGHT = 50  # Высота
+BLOCKS_DIST = 20  # Интервал между блоками
+BLOCKS_LENGTH = BLOCKS_WIDTH + BLOCKS_DIST  # Расчет длины одного блока
+BLOCKS_COUNT = WIDTH // BLOCKS_LENGTH  # Расчет количества блоков на экране
 
-# buttons settings
-BUTTON_W = 300
-BUTTON_H = 100
+# Настройки кнопок
+BUTTON_W = 300  # Ширина
+BUTTON_H = 100  # Высота
